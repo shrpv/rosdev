c82837e549e0f95748af2c44d217dc49ff0206fd
Dmitry Sharipov <dmitriy.sharipov@jugru.org>
2024-11-13 15:54:16 +0500

    refactor(page): refactored component

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,36 @@
 import styles from "./page.module.scss";
-import { Hero } from "../components/Hero/Hero";
-import { Advantages } from "../components/Advantages/Advantages";
-import { Platforms } from "../components/Platforms/Platforms";
-import { Services } from "../components/Services/Services";
-import { Technologies } from "../components/Technologies/Technologies";
-import { Clients } from "../components/Clients/Clients";
-import { Reviews } from "../components/Reviews/Reviews";
-import { About } from "../components/About/About";
-import { Slogan } from "../components/Slogan/Slogan";
+import { Hero } from "@/components/Hero/Hero";
+import { Advantages } from "@/components/Advantages/Advantages";
+import { Platforms } from "@/components/Platforms/Platforms";
+import { Services } from "@/components/Services/Services";
+import { Technologies } from "@/components/Technologies/Technologies";
+import { Clients } from "@/components/Clients/Clients";
+import { Reviews } from "@/components/Reviews/Reviews";
+import { About } from "@/components/About/About";
+import { Slogan } from "@/components/Slogan/Slogan";
 
 export default function Page() {
     return (
         <>
-            {/* <Hero className={styles.home__hero} /> */}
+            <Hero className={styles.home__hero} />
 
-            <Advantages className={styles.home__advantages} />
+            <div id="content">
+                <Advantages className={styles.home__advantages} />
 
-            <Platforms className={styles.home__platforms} />
+                <Platforms className={styles.home__platforms} />
 
-            <Services className={styles.home__services} />
+                <Services className={styles.home__services} />
 
-            <Technologies className={styles.home__technologies} />
+                <Technologies className={styles.home__technologies} />
 
-            <Clients className={styles.home__clients} />
+                <Clients className={styles.home__clients} />
 
-            <Reviews className={styles.home__reviews} />
+                <Reviews className={styles.home__reviews} />
 
-            <About />
+                <About />
 
-            <Slogan className={styles.home__slogan} />
+                <Slogan className={styles.home__slogan} />
+            </div>
         </>
     );
 }

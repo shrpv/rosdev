8f86b6e217f7b3d998e5e45cd56254229a7c1025
Dmitry Sharipov <dmitriy.sharipov@jugru.org>
2024-11-13 15:51:08 +0500

    refactor(Footer, HeaderMenu): refactored components import urls

--- src/components/Utils/Header/HeaderMenu/HeaderMenu.tsx
+++ src/components/Utils/Header/HeaderMenu/HeaderMenu.tsx
@@ -2,9 +2,9 @@ import { FC, MouseEventHandler } from "react";
 import styles from "./HeaderMenu.module.scss";
 import { HeaderNavigation } from "../HeaderNavigation/HeaderNavigation";
 import { HeaderMenuToggle } from "./HeaderMenuToggle/HeaderMenuToggle";
-import { Contacts } from "../../Contacts/Contacts";
-import { Copyright } from "../../Copyright/Copyright";
-import { Location } from "../../Location/Location";
+import { Contacts } from "@/components/Utils/Contacts/Contacts";
+import { Copyright } from "@/components/Utils/Copyright/Copyright";
+import { Location } from "@/components/Utils/Location/Location";
 
 interface HeaderMenuProps {
     className?: string;

05d45730ee64ed9e5cdb0eca66f1351cc7040340
Dmitry Sharipov <dmitriy.sharipov@jugru.org>
2024-11-13 15:52:54 +0500

    feat(SmoothScrollLayout): added new layout

---
+++ src/components/SmoothScrollLayout/SmoothScrollLayout.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { FC, ReactNode, useEffect, useRef } from "react";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Main } from "@/components/Utils/Main/Main";
+import { Header } from "@/components/Utils/Header/Header";
+import { Footer } from "@/components/Utils/Footer/Footer";
+import { ScrollToTopButton } from "@/components/ScrollToTopButton/ScrollToTopButton";
+
+interface SmoothScrollLayoutProps {
+    children?: ReactNode;
+}
+
+export const SmoothScrollLayout: FC<SmoothScrollLayoutProps> = ({ children }) => {
+    const lenisRef = useRef();
+    const lenis = useLenis(({ scroll }) => {});
+    const target = useRef();
+    const timeline = useRef();
+
+    useEffect(() => {
+        function update(time) {
+            lenisRef.current?.lenis?.raf(time * 1000);
+        }
+
+        gsap.ticker.add(update);
+
+        return () => {
+            gsap.ticker.remove(update);
+        };
+    });
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        // noinspection TypeScriptValidateTypes
+        timeline.current = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#hero",
+                scrub: true,
+                start: "bottom bottom",
+                snap: {
+                    duration: 0.3
+                }
+            }
+        });
+
+        timeline.current.fromTo(
+            target.current,
+            { opacity: 0, y: 40, hidden: true },
+            { opacity: 1, y: 0, hidden: false }
+        );
+
+        return () => {
+            // noinspection TypeScriptUnresolvedReference
+            timeline?.current?.kill();
+        };
+    }, []);
+
+    // noinspection TypeScriptValidateTypes
+    return (
+        <ReactLenis
+            root
+            ref={lenisRef}
+            autoRaf={false}
+            options={{
+                lerp: 0.02,
+                easing: (t: number) => Math.min(1, 1.001 - 2 ** (-10 * t)),
+                orientation: "vertical",
+                gestureOrientation: "vertical",
+                smoothWheel: true,
+                wheelMultiplier: 1,
+                smoothTouch: true,
+                touchMultiplier: 2
+            }}
+        >
+            <Header className="page__header" />
+
+            <Main className="page__main">{children}</Main>
+
+            <Footer className="page__footer" />
+
+            <ScrollToTopButton
+                ref={target}
+                onClick={() => {
+                    lenis.scrollTo("#main", { lerp: 0.02 });
+                }}
+            />
+        </ReactLenis>
+    );
+};
